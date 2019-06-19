abe58453370cb734f116b32db46ad6512a6a2a4e
kasterni <38432794+kasterni@users.noreply.github.com>
2019-06-19 13:41:56 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -223,3 +223,119 @@ jQuery(".toggle .toggle-title").click(function(){
 // form
 
 
+// hover social media
+
+var hoverMouse = function($el) {
+  $el.each(function() {
+    var $self = $(this);
+    var hover = false;
+    var offsetHoverMax = $self.attr("offset-hover-max") || 0.7;
+    var offsetHoverMin = $self.attr("offset-hover-min") || 0.5;
+
+    var attachEventsListener = function() {
+      $(window).on("mousemove", function(e) {
+        //
+        var hoverArea = hover ? offsetHoverMax : offsetHoverMin;
+
+        // cursor
+        var cursor = {
+          x: e.clientX,
+          y: e.clientY - $(window).scrollTop()
+        };
+
+        // size
+        var width = $self.outerWidth();
+        var height = $self.outerHeight();
+
+        // position
+        var offset = $self.offset();
+        var elPos = {
+          x: offset.left + width / 2,
+          y: offset.top + height / 2
+        };
+
+        // comparaison
+        var x = cursor.x - elPos.x;
+        var y = cursor.y - elPos.y;
+
+        // dist
+        var dist = Math.sqrt(x * x + y * y);
+
+        // mutex hover
+        var mutHover = false;
+
+        // anim
+        if (dist < width * hoverArea) {
+          mutHover = true;
+          if (!hover) {
+            hover = true;
+          }
+          onHover(x, y);
+        }
+
+        // reset
+        if (!mutHover && hover) {
+          onLeave();
+          hover = false;
+        }
+      });
+    };
+
+    var onHover = function(x, y) {
+      TweenMax.to($self, 0.4, {
+        x: x * 0.8,
+        y: y * 0.8,
+        //scale: .9,
+        rotation: x * 0.05,
+        ease: Power2.easeOut
+      });
+    };
+    var onLeave = function() {
+      TweenMax.to($self, 0.7, {
+        x: 0,
+        y: 0,
+        scale: 1,
+        rotation: 0,
+        ease: Elastic.easeOut.config(1.2, 0.4)
+      });
+    };
+
+    attachEventsListener();
+  });
+};
+
+hoverMouse($('span'));
+
+
+// parallax mouse move
+
+$.fn.parallax = function(resistance, mouse) {
+  $el = $(this);
+  TweenLite.to($el, 0.2, {
+    x: -((mouse.clientX - window.innerWidth / 2) / resistance),
+    y: -((mouse.clientY - window.innerHeight / 2) / resistance)
+  });
+};
+
+$(document).mousemove(function(e) {
+  $(".img-block").parallax(-40, e);
+  $(".img-parallax").parallax(20, e);
+  });
+
+
+
+  $(document).mousemove(function(e) {
+    parallaxIt(e, ".img-parallax-left", -40);
+    parallaxIt(e, ".img-parallax-right", -20);
+  });
+  
+  function parallaxIt(e, target, movement) {
+    var $this = $("img");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+  
+    TweenMax.to(target, 1, {
+      x: (relX - $this.width() / 2) / $this.width() * movement,
+      y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+  }
